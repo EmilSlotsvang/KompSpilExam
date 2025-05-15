cf61735496627bf9ddd283ff7e9a9f127a4417e2
EmilSlotsvang <emil.slotsvang@gmail.com>
2025-05-15 15:13:14 +0200

    Spring framework

--- Core/src/main/java/emil/komp/asteroids/main/Main.java
+++ Core/src/main/java/emil/komp/asteroids/main/Main.java
@@ -11,24 +11,135 @@ import javafx.scene.Scene;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Polygon;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
-import emil.komp.asteroids.main.ServiceLoaderHelper;
-import java.util.Collection;
+
 import java.util.Map;
-import java.util.ServiceLoader;
 import java.util.concurrent.ConcurrentHashMap;
 
-
-
-
-import static java.util.stream.Collectors.toList;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 
 
 public class Main extends Application {
 
-    private  final ServiceLoaderHelper serviceLoaderHelper = new ServiceLoaderHelper();
+    private final Gamedata gameData = new Gamedata();
+    private final World world = new World();
+    private final Map<Entity, Polygon> polygons = new ConcurrentHashMap<>();
+    private final Pane gameWindow = new Pane();
+
+    private Iterable<IGamePluginService> gamePluginServices;
+    private Iterable<IEntityProcessingService> entityProcessingServices;
+    private Iterable<IPostEntityProcessingService> postEntityProcessingServices;
+
+    public static void main(String[] args) {
+        launch(Main.class);
+    }
+
+    @Override
+    public void start(Stage window) throws Exception {
+        // Initialize Spring application context
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SpringConf.class);
+
+        // Retrieve beans from SpringConfig
+        gamePluginServices = context.getBean("gamePluginServices", Iterable.class);
+        entityProcessingServices = context.getBean("entityProcessingServices", Iterable.class);
+        postEntityProcessingServices = context.getBean("postEntityProcessingServices", Iterable.class);
+
+
+        gameWindow.setPrefSize(gameData.getDisplayWidth(), gameData.getDisplayHeight());
+
+
+        Scene scene = new Scene(gameWindow);
+        scene.setOnKeyPressed(event -> {
+            if (event.getCode().equals(KeyCode.LEFT)) {
+                gameData.getKeys().setKey(GameKeys.LEFT, true);
+            }
+            if (event.getCode().equals(KeyCode.RIGHT)) {
+                gameData.getKeys().setKey(GameKeys.RIGHT, true);
+            }
+            if (event.getCode().equals(KeyCode.UP)) {
+                gameData.getKeys().setKey(GameKeys.UP, true);
+            }
+            if (event.getCode().equals(KeyCode.SPACE)) {
+                gameData.getKeys().setKey(GameKeys.SPACE, true);
+            }
+        });
+        scene.setOnKeyReleased(event -> {
+            if (event.getCode().equals(KeyCode.LEFT)) {
+                gameData.getKeys().setKey(GameKeys.LEFT, false);
+            }
+            if (event.getCode().equals(KeyCode.RIGHT)) {
+                gameData.getKeys().setKey(GameKeys.RIGHT, false);
+            }
+            if (event.getCode().equals(KeyCode.UP)) {
+                gameData.getKeys().setKey(GameKeys.UP, false);
+            }
+            if (event.getCode().equals(KeyCode.SPACE)) {
+                gameData.getKeys().setKey(GameKeys.SPACE, false);
+            }
+        });
+
+        // Use Spring beans instead of ServiceLoaderHelper
+        for (IGamePluginService iGamePlugin : gamePluginServices) {
+            iGamePlugin.start(gameData, world);
+        }
+        for (Entity entity : world.getEntities()) {
+            Polygon polygon = new Polygon(entity.getPolygonCoordinates());
+            polygons.put(entity, polygon);
+            gameWindow.getChildren().add(polygon);
+        }
+        render();
+        window.setScene(scene);
+        window.setTitle("ASTEROIDS");
+        window.show();
+    }
+
+    private void render() {
+        new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                update();
+                draw();
+                gameData.getKeys().update();
+            }
+        }.start();
+    }
+
+    private void update() {
+        for (IEntityProcessingService entityProcessorService : entityProcessingServices) {
+            entityProcessorService.process(gameData, world);
+        }
+        for (IPostEntityProcessingService postEntityProcessorService : postEntityProcessingServices) {
+            postEntityProcessorService.process(gameData, world);
+        }
+    }
+
+    private void draw() {
+        for (Entity polygonEntity : polygons.keySet()) {
+            if (!world.getEntities().contains(polygonEntity)) {
+                Polygon removedPolygon = polygons.get(polygonEntity);
+                polygons.remove(polygonEntity);
+                gameWindow.getChildren().remove(removedPolygon);
+            }
+        }
+
+        for (Entity entity : world.getEntities()) {
+            Polygon polygon = polygons.get(entity);
+            if (polygon == null) {
+                polygon = new Polygon(entity.getPolygonCoordinates());
+                polygons.put(entity, polygon);
+                gameWindow.getChildren().add(polygon);
+            }
+            polygon.setTranslateX(entity.getX());
+            polygon.setTranslateY(entity.getY());
+            polygon.setRotate(entity.getRotation());
+        }
+    }
+}
+/*
+public class Main extends Application {
+
+    private  final SpringConf serviceLoaderHelper = new SpringConf();
     private final Gamedata gamedata = new Gamedata();
 
     private final World world = new World();
@@ -140,3 +251,5 @@ public class Main extends Application {
 
         }
     }
+
+ */
--- Core/src/main/java/module-info.java
+++ Core/src/main/java/module-info.java
@@ -1,11 +1,22 @@
+import emil.komp.asteroids.common.services.IEntityProcessingService;
+import emil.komp.asteroids.common.services.IGamePluginService;
+import emil.komp.asteroids.common.services.IPostEntityProcessingService;
+
 module Core {
     requires Common;
     requires javafx.graphics;
 
-    opens emil.komp.asteroids.main to javafx.graphics;
+    requires spring.context;
+        requires spring.core;
+    requires spring.beans;
+
+    exports emil.komp.asteroids.main to spring.context;
+
+    opens emil.komp.asteroids.main to javafx.graphics, spring.core, spring.beans;
 
     uses emil.komp.asteroids.common.services.IEntityProcessingService;
     uses emil.komp.asteroids.common.services.IGamePluginService;
     uses emil.komp.asteroids.common.services.IPostEntityProcessingService;
+
 
 }
