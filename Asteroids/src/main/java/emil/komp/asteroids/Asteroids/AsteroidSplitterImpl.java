620baccb89b146da36c67e46ce6d463ffb8eac4c
EmilSlotsvang <emil.slotsvang@gmail.com>
2025-05-16 14:32:02 +0200

    minor fix

--- Asteroids/src/main/java/emil/komp/asteroids/Asteroids/AsteroidSplitterImpl.java
+++ Asteroids/src/main/java/emil/komp/asteroids/Asteroids/AsteroidSplitterImpl.java
@@ -9,6 +9,7 @@ public class AsteroidSplitterImpl implements IAsteroidSplitter {
 
     @Override
     public void createAsteroids(Entity original, World world) {
+
         float newRadius = original.getRadius() /2;
 
         if (newRadius < 3) return;
@@ -16,8 +17,11 @@ public class AsteroidSplitterImpl implements IAsteroidSplitter {
         for (int i = 0; i < 2; i++) {
             Entity smallAsteroid = new asteroid();
             smallAsteroid.setRadius(newRadius);
-            smallAsteroid.setX(original.getX() + (Math.random() - 0.5) * 10);
-            smallAsteroid.setY(original.getY() + (Math.random() - 0.5) * 10);
+            double angle = Math.random() * 2 * Math.PI;
+            double distance = original.getRadius() + 5; // Garanteret uden for kollisionsafstand
+
+            smallAsteroid.setX(original.getX() + Math.cos(angle) * distance);
+            smallAsteroid.setY(original.getY() + Math.sin(angle) * distance);
             smallAsteroid.setRotation((int) (Math.random() * 360));
             smallAsteroid.setPolygonCoordinates(
                     newRadius, -newRadius, -newRadius, -newRadius, -newRadius, newRadius, newRadius, newRadius
